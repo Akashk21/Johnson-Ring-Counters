3ebf123e9532fcb849c6f0a3ae5e036bfe610718
Akashk21 <71625383+Akashk21@users.noreply.github.com>
2022-03-09 22:44:23 +0530

    Add files via upload

---
+++ DDCO_Counters/john_tb.v
@@ -0,0 +1,35 @@
+module tb_john;
+
+    // Inputs
+    reg Clock;
+    reg Reset;
+
+    // Outputs
+    wire [3:0] Count_out;
+
+    // Instantiate the Unit Under Test (UUT)
+    johnson_counter uut (
+        .Clock(Clock), 
+        .Reset(Reset), 
+        .Count_out(Count_out)
+    );
+
+    ///////////////////Clock generation ///////////////////////////
+    initial Clock = 0; 
+    always #10 Clock = ~Clock; 
+    ////////// #10 means wait for 10 ns before executing the next statement. ///////////
+    
+    //Simulation inputs.
+    initial begin
+    //Apply Reset for 50 ns.
+        Reset = 1; //Reset is high
+        #10;       //Wait for 50 ns
+        Reset = 0; //Reset is low.
+        #150 $finish;
+    end
+    initial begin
+          $monitor($time, " clock=%1b,reset=%1b,q=%4b",Clock,Reset,Count_out);
+		end
+		initial begin $dumpfile("tb_johnsontest.vcd"); $dumpvars(0, tb_john); end
+      
+endmodule
